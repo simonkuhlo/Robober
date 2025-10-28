80bbeecd4acd4060d88b2e44330c9735799edefd
Simon <siga241@gmail.com>
2025-10-28 09:34:38 +0100

    update channel access view

--- Plugins/ChannelCloner/Res/Views/channel_access_view.py
+++ Plugins/ChannelCloner/Res/Views/channel_access_view.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import discord
 from discord import VoiceChannel
+from ... import channel_access_worker
 
 class AcceptMode(Enum):
     ACCEPT = "✅ Accept"
@@ -13,7 +14,6 @@ class ChannelAccessView(discord.ui.View):
         self.target_channel = target_channel
         self.accept_mode:AcceptMode = AcceptMode.ACCEPT
         self.add_item(AcceptRejectSelect())
-        #self.add_item(UserSelect(self))
 
 class AcceptRejectSelect(discord.ui.Select):
     def __init__(self):
@@ -35,10 +35,16 @@ class AcceptRejectSelect(discord.ui.Select):
                 mode = AcceptMode.ACCEPT
             case "Reject User":
                 mode = AcceptMode.REJECT
-        await interaction.response.send_message(f"You selected {mode.value}.")
+        await interaction.response.send_message(f"You selected {mode.value}.", ephemeral=True, view=UserSelectView(mode))
+
+class UserSelectView(discord.ui.View):
+    def __init__(self, accept_mode: AcceptMode):
+        super().__init__(timeout=None)
+        self.add_item(UserSelect(accept_mode))
 
 class UserSelect(discord.ui.UserSelect):
-    def __init__(self, mode: AcceptMode):
+    def __init__(self, accept_mode: AcceptMode):
+        self.accept_mode = accept_mode
         super().__init__(
             placeholder="Select a user",
             min_values=1,
@@ -49,8 +55,17 @@ class UserSelect(discord.ui.UserSelect):
         selected_users = self.values
         # If mode is selected, do action instantly, else cache selection and wait for mode select
         mention_string:str = ""
+        allowed:bool = None
+        match self.accept_mode:
+            case AcceptMode.ACCEPT:
+                allowed = True
+            case AcceptMode.REJECT:
+                allowed = False
         for selected_user in selected_users:
             mention_string += ", " + selected_user.mention
+            overwrite = interaction.channel.overwrites_for(selected_user)
+            overwrite.connect = allowed
+            await interaction.channel.set_permissions(selected_user, overwrite=overwrite)
         await interaction.response.send_message(
             f"You selected {mention_string}",
             ephemeral=True
--- Plugins/ChannelCloner/channel_access_worker.py
+++ Plugins/ChannelCloner/channel_access_worker.py
@@ -1,10 +1,8 @@
+import discord
+
 from SimonsPluginResources.Main.reelbot import ReelBot
 
-async def change_user_channel_access(user_id:int, channel_id:int, bot:ReelBot, allowed:bool = None) -> None:
-    target_channel = bot.get_channel(channel_id)
-    member = target_channel.guild.get_member(user_id)
+async def change_user_channel_access(member:discord.Member, channel:discord.VoiceChannel, allowed:bool = None) -> None:
     if not member:
         return
-    overwrite = target_channel.overwrites_for(member)
-    overwrite.connect = allowed
-    await target_channel.set_permissions(member, overwrite=overwrite)
+
