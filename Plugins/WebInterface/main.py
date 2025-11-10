4b8a167193c8a40d90d37041a69f1615300fee40
Simon <siga241@gmail.com>
2025-11-10 13:49:29 +0100

    refactor webinterface view to fit into webinterface extension

--- Plugins/WebInterface/main.py
+++ Plugins/WebInterface/main.py
@@ -15,27 +15,25 @@ class WebInterfacePluginExtension(PluginExtension):
     def __init__(self, parent_plugin: "WebInterfacePlugin" = None):
         super().__init__(parent_plugin)
         visual.extension = self
+        # Get the directory where the current file (e.g., main.py) is located
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        # Construct the path to the static directory relative to BASE_DIR
+        static_dir_path = os.path.join(BASE_DIR, "visual/static")
+
+        # --- FastAPI Web Server Setup ---
+        self.app = FastAPI()
+        self.app.mount("/visual/static", StaticFiles(directory=static_dir_path), name="static")
+        self.app.include_router(visual.router)
+
+        @self.app.get("/")
+        async def read_root():
+            return RedirectResponse(url="/visual", status_code=308)
+
+    # Function to run the FastAPI server in a separate thread
+    def run_webinterface(self):
+        uvicorn.run(self.app, host="localhost", port=8000)
+
     def _start(self) -> None:
-        on_startup()
+        api_thread = Thread(target=self.run_webinterface, daemon=True)
+        api_thread.start()
 
-# Get the directory where the current file (e.g., main.py) is located
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# Construct the path to the static directory relative to BASE_DIR
-static_dir_path = os.path.join(BASE_DIR, "visual/static")
-
-# --- FastAPI Web Server Setup ---
-app = FastAPI()
-app.mount("/visual/static", StaticFiles(directory=static_dir_path), name="static")
-app.include_router(visual.router)
-
-@app.get("/")
-async def read_root():
-    return RedirectResponse(url="/visual", status_code=308)
-
-# Function to run the FastAPI server in a separate thread
-def run_webinterface():
-    uvicorn.run(app, host="localhost", port=8000)
-
-def on_startup() -> None:
-    api_thread = Thread(target=run_webinterface, daemon=True)
-    api_thread.start()
