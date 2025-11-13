958b480da17b536e572751da09cbc40d275c1b51
Gander Simon <siga241@gmail.com>
2025-11-13 21:48:40 +0100

    use new settings concept

--- Plugins/ChannelCloner/plugin.py
+++ Plugins/ChannelCloner/plugin.py
@@ -1,8 +1,7 @@
 from SimonsPluginResources.environment import Environment
 from SimonsPluginResources.plugin import Plugin
-from SimonsPluginResources.settings.setting import Setting
-from SimonsPluginResources.settings.scopes import PluginScope
-from SimonsPluginResources.settings.type_filters import filters
+from SimonsPluginResources.settings import Setting
+from SimonsPluginResources.settings.models.scope import ScopePlugin
 from .cog import ChannelCloner
 from . import channel_authority_manager
 
@@ -16,25 +15,12 @@ class ChannelClonerPlugin(Plugin):
                          used_host_version= 0,
                          cogs = [ChannelCloner],
                          )
-        self.origin_channel_setting: Setting = Setting(
-                                                    "origin_channel",
-                                                    "id",
-                                                    "Channelcloner",
-                                                    type_filter=filters.INT,
-                                                    scope=PluginScope(self.plugin_id),
-                                                    default_value="963376060672647169"
-                                                    )
-        self.temp_category_setting: Setting = Setting(
-                                                    "temp_channel_category",
-                                                    "id",
-                                                    "Channelcloner",
-                                                    type_filter=filters.INT,
-                                                    scope=PluginScope(self.plugin_id),
-                                                    default_value="1361792521226948650"
-                                                    )
 
     def get_settings(self) -> list[Setting]:
-        return [self.origin_channel_setting, self.temp_category_setting]
+        return [
+                Setting(rel_path="origin_channel.id", default_value="963376060672647169", scope=ScopePlugin(plugin_id=self.plugin_id)),
+                Setting(rel_path="temp_category.id", default_value="1361792521226948650", scope=ScopePlugin(plugin_id=self.plugin_id)),
+                ]
 
     def get_active_channel_ids(self) -> list[int]:
         return channel_authority_manager.get_active_channel_ids()
--- core_settings.py
+++ core_settings.py
@@ -1,28 +1,7 @@
-from SimonsPluginResources.settings.setting import Setting
-from SimonsPluginResources.settings.type_filters import filters
+from SimonsPluginResources.settings import Setting
 
 initial_settings:list[Setting] = [
-    Setting(topic="logging",
-            setting_id="level",
-            category="Logging",
-            description="",
-            type_filter=filters.INT,
-            important=True,
-            default_value="0",
-            ),
-    Setting(topic="debug_server",
-            setting_id="id",
-            category="Debugging",
-            description="",
-            type_filter=filters.INT,
-            important=True,
-            default_value="954128852051968070",
-            ),
-    Setting(topic="commands",
-            setting_id="trigger",
-            category="Bot",
-            description="",
-            important=True,
-            default_value="!r",
-            )
+    Setting(rel_path="logging.level", default_value="0"),
+    Setting(rel_path="debug_guild.id", default_value="954128852051968070"),
+    Setting(rel_path="commands.trigger", default_value="!r"),
 ]
