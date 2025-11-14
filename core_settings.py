6550f5710f4bc1597e99458ad030110e6d5726f7
Simon <siga241@gmail.com>
2025-11-14 16:26:43 +0100

    add setting comments

--- core_settings.py
+++ core_settings.py
@@ -2,6 +2,6 @@ from SimonsPluginResources.settings import Setting
 
 initial_settings:list[Setting] = [
     Setting(rel_path="logging.level", default_value="0"),
-    Setting(rel_path="debug_guild.id", default_value="954128852051968070"),
-    Setting(rel_path="commands.trigger", default_value="!r"),
+    Setting(rel_path="debug_guild.id", default_value="954128852051968070", comment="Bot must be restarted after changing value!"),
+    Setting(rel_path="commands.trigger", default_value="!r", comment="Bot must be restarted after changing value!"),
 ]
