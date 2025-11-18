04212d56ef4507a7bc2566fffa133e7131b3bfdb
simonkuhlo <siga241@gmail.com>
2025-11-18 01:13:32 +0100

    update to match new asynctask layout

--- bare_bones_test.py
+++ bare_bones_test.py
@@ -18,7 +18,7 @@ async def main():
     bot: ReelBot = ReelBot(logger, settings)
     env: Environment = Environment(settings, logger, task_manager, bot)
     plugin_host: PluginHost = PluginHost(env)
-    task_manager.add_task(AsyncTask(bot.start(get_key(".env.", "BOT_TOKEN")), "Bot main loop"))
+    task_manager.add_task(AsyncTask(bot.start, args=(get_key(".env", "BOT_TOKEN"),), name="Bot main loop"))
     await plugin_host.add_plugin(plugin2.WebInterfacePlugin)
     await plugin_host.add_plugin(ChannelClonerPlugin)
     await task_manager.start()
